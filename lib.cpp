d256bad8f514fe06c4069c9f0e9d4eeb6a91e9fe
Oleksandra <sanxln@gmail.com>
2025-03-07 10:19:47 +0200

    add numbers

--- lib.cpp
+++ lib.cpp
@@ -4,4 +4,12 @@ int main(){
     for(int i = 0; i < 10; ++i){
         std::cout <<i;
     }
+
+    int number;
+    std::cout <<"Enter a number";
+    std::cin >> number;
+
+    if( number > 0){
+        std::cout << "True";
+    }
 }
