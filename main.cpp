a6ac91a417955b03e8fee8173f2d1f5ca21dd5d3
Oleksandra <sanxln@gmail.com>
2025-03-20 16:20:07 +0200

    Add sum functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<map>
 
+
+int sum(int a, int b) {
+   return a + b;
+}
+
 int main(){
  std::cout <<"Hello word";
 
@@ -10,5 +15,9 @@ int main(){
  std::cout <<"  ";
 
  std::map<int, int> m;
- 
+ std::cout << "m";
+
+ std::cout << sum(3, 5); 
+ return 0;
+
 }
