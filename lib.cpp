7f06df37f576349525ae451771751c933b053f20
Oleksandra <sanxln@gmail.com>
2025-03-26 10:20:00 +0200

    Create vector

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<vector>
 
 int main(){
     for(int i = 0; i < 10; ++i){
         std::cout <<i;
     }
 
+    std::vector<int> vec;
     int number;
     std::cout <<"Enter a number";
     std::cin >> number;
