61ecc466774041a16d2b0cfbff99360c53ff60be
Oleksandra <sanxln@gmail.com>
2025-03-26 09:58:01 +0200

    Add coments on main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int main(){
     std::cout << "It's true";
  }
  std::cout <<"  ";
-
+//This functions vector
  std::vector<int> vec;
  for(int i = 0; i < 0; i++){
    std::cout << i;
  }
+ return 0;
 }
